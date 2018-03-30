6af45c5468ada195c839f054defc73fbefa77636
DH-TB <929659475@qq.com>
2018-03-30 16:27:16 +0800

    finish java-pre-pos/
    Signed-off-by: DH-TB <929659475@qq.com>

---
+++ java-pre-pos/src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -0,0 +1,17 @@
+package cn.school.thoughtworks.section3;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PracticeB {
+    Map<String, Integer> createUpdatedCollection(Map<String, Integer> collectionA, Map<String, List<String>> object) {
+        //实现练习要求，并改写该行代码。
+        List<String> collections = object.get("value");
+        for (String c : collections) {
+            Integer num = collectionA.get(c);
+            collectionA.put(c, num-num/3);
+        }
+        return collectionA;
+    }
+}
