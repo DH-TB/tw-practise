a879a128f25c6515dae8e20c7f31cc9eda85d645
DH-TB <929659475@qq.com>
2018-03-30 15:06:21 +0800

    test
    Signed-off-by: DH-TB <929659475@qq.com>

---
+++ spring-data-jpa/src/main/java/com/example/employee/entity/Employee.java
@@ -0,0 +1,98 @@
+package com.example.employee.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Integer age;
+
+    @NotNull
+    private String gender;
+
+    @NotNull
+    private Integer salary;
+
+    @NotNull
+    private Integer companyId;
+
+    public Employee(String name, Integer age, String gender, Integer salary, Integer companyId) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.companyId = companyId;
+    }
+
+    public Employee(String name, Integer age, String gender, Integer salary, Integer companyId, Integer id) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+        this.companyId = companyId;
+        this.id = id;
+    }
+
+    public Employee(){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+}
