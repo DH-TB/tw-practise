954295a981db460a3825709b0a8930d59ddc7d5f
DH-TB <929659475@qq.com>
2018-04-02 12:41:09 +0800

    finish spring-boot
    Signed-off-by: DH-TB <929659475@qq.com>

---
+++ spring-boot/out/production/resources/db/migration/V2__insert_employee.sql
@@ -0,0 +1,5 @@
+INSERT INTO Employee (id,name,age,gender) VALUES ( 1, '小明',20,'男');
+INSERT INTO Employee (id,name,age,gender) VALUES ( 2, '小红',19,'女');
+INSERT INTO Employee (id,name,age,gender) VALUES ( 3, '小智',15,'男');
+INSERT INTO Employee (id,name,age,gender) VALUES ( 4, '小刚',16,'男');
+INSERT INTO Employee (id,name,age,gender) VALUES ( 5, '小霞',15,'女');
---
+++ spring-boot/src/main/java/com/example/demo/controller/EmployeesController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/employee")
+public class EmployeesController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("")
+    public ResponseEntity getAll() {
+        List<Employee> employees = employeeRepository.findAll();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+}
